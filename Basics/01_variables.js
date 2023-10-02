dc1bdde1a7e3c982f900f903587b62f147afc6a6
Sanket Mane <111991599+Sanketmane2323@users.noreply.github.com>
2023-10-02 11:28:44 +0530

    Basic of js

---
+++ Basics/01_variables.js
@@ -0,0 +1,12 @@
+const account_no =234524;
+
+let accountName="sanket"
+
+var accountEmail="sanketmane2323@gmail.com"
+
+console.log(account_no)
+
+console.table([accountEmail,accountName,account_no])
+
+
+//Never use the var keyword for declaring the varibales 
---
+++ Basics/02_datatypes.js
@@ -0,0 +1,12 @@
+let p_name="sanket"
+
+let age=18
+
+let isLoggedIn=true
+
+// string-""
+// BigInt generally used when number is too big
+// boolean
+//null - standalone value
+
+console.log(typeof null);
